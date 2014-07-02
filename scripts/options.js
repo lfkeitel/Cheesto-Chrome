65e871feaa1a4df1bc21bc6eef74be145ddc55f5
dragonrider23_ <keitell@gmail.com>
2014-07-02 09:31:37 -0500

    Added color to status message

--- scripts/options.js
+++ scripts/options.js
@@ -4,10 +4,13 @@ chrome.runtime.onMessage.addListener(function(request) { handleWebError(request.
 
 function handleWebError(details) {
   if (details.error == "net::ERR_CONNECTION_REFUSED") {
-    displayStatus('Error validating API key. Connection Refused');
+    displayStatus('Error validating API key. Connection Refused', 'red');
   }
   else if (details.error == "net::ERR_NAME_NOT_RESOLVED") {
-    displayStatus('Invalid hostname. Name Not Resolved');
+    displayStatus('Invalid hostname. Name Not Resolved', 'red');
+  }
+  else {
+    displayStatus('Error: ' + details.error, 'red');
   }
 }
 
@@ -31,14 +34,15 @@ function save_options() {
           });
         }
         else if (result.errorcode == 1) {
-            displayStatus('API key is invalid');
+            displayStatus('API key is invalid', 'red');
         }
       }
       else if (xhr.status == 404) {
-        displayStatus('Error validating API key. File Not Found');
+        displayStatus('Error validating API key. File Not Found.<br>\
+          Is the path correct? Are you using Dandelion version 5+?', 'red');
       }
       else {
-        displayStatus('Error: ' + xhr.statusText);
+        displayStatus('Error: ' + xhr.statusText, 'red');
       }
     }
   }
@@ -46,13 +50,16 @@ function save_options() {
   xhr.send("apikey="+apikey);
 }
 
-function displayStatus(message) {
+function displayStatus(message, classColor) {
+  classColor = typeof classColor !== 'undefined' ? classColor : '';
   var status = document.getElementById('status');
 
-  status.textContent = message;
+  status.innerHTML = message;
+  status.className = classColor;
 
   setTimeout(function() {
     status.textContent = '';
+    status.className = '';
   }, 5000);
 }
 
